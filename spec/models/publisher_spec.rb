f6f815a4f3f9d1e404ee926dab26b9e511bf922e
patrick204nqh <patrick204nqh@gmail.com>
2020-11-26 10:24:05 +0700

    setup testing environment

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+
+  it 'has a valid factory' do
+    expect(build(:publisher)).to be_valid
+  end
 end
